e8d9a0ac4982dc630ef08cae2b4428446fc7ef30
Natã Vitoriano de Almeida Silva <natavitoriano@hotmail.com>
2021-02-08 11:12:08 -0300

    Adding the folder that will contain the function material

---
+++ Funcao/cidadaoPrimeiraClasse.js
@@ -0,0 +1,37 @@
+//Função em Js é Fist-Class Object (Citizens)
+//Higher-order function
+
+//Criar de forma literal
+function fun1(){ }
+
+//armazenar em uma variável
+
+const fun2 = function () { }
+
+//Armazenar em um array
+const array = [function (a,b) {return a + b}, fun1, fun2]
+console.log(array[0](2,3))
+
+//Armazenar em um atributo de objeto
+const obj = {}
+obj.falar = function() {return 'Opa'}
+console.log(obj.falar())
+
+//Passar função como parametro
+function run(fun){
+    fun()
+}
+
+run(function () {console.log('Executando...')})
+
+//uma Funcação pode retornar/conter uma função
+function soma(a,b){
+    return function (c){
+        console.log(a + b + c)
+    }
+}
+
+soma(2,3)(4)
+
+const cincoMais = soma(2,3)
+cincoMais(4)
